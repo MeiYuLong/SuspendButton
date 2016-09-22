6b270a90e48d268e754262225aa23c694322b187
yulong mei <longjiao914@126.com>
2016-09-22 17:12:04 +0800

    优化横屏

--- SuspendButton/SuspendButton.m
+++ SuspendButton/SuspendButton.m
@@ -7,6 +7,7 @@
 //
 
 #import "SuspendButton.h"
+#import "AppDelegate.h"
 
 #define Screen_Hight [[UIScreen mainScreen] bounds].size.height//屏高
 
@@ -37,6 +38,8 @@ typedef enum{
     UITouch *touch = [touches anyObject];
     
     _startPos = [touch locationInView:_rootView];
+    
+    _startPos = [self converDir:_startPos];
 }
 
 //手指按住移动过程，通过悬浮按钮的拖动事件来拖动整个悬浮窗口
@@ -46,7 +49,7 @@ typedef enum{
     UITouch *touch = [touches anyObject];
     
     //移动按钮到当前触摸位置
-    self.superview.center = [touch locationInView:_rootView];
+    self.superview.center = [self converDir:[touch locationInView:_rootView]];
 }
 
 //拖动结束后是悬浮窗口吸附在最近的屏幕边缘
@@ -56,12 +59,25 @@ typedef enum{
     
     CGPoint curPoint = [touch locationInView:_rootView];
     
+    curPoint = [self converDir:curPoint];
+    
     //通知代理，如果结束触点和起始点极近就认为是点击事件
     if (pow((_startPos.x - curPoint.x),2) + pow((_startPos.y - curPoint.y), 2) <1) {
         
         [self.buttonDelegate dragButtonClicked:self];
         
         return;//点击后不动
+    }
+    
+    //由于计算吸附时，坐标要转换到手机竖直状态不旋转的坐标系，因此要保障屏幕宽度是竖直状态的快读，高度也是竖直状态的高度，不随屏幕的旋转二变化
+    //获取屏幕的方向
+    UIInterfaceOrientation  orientation = [UIApplication sharedApplication].statusBarOrientation;
+    CGFloat W = Screen_Width;
+    CGFloat H = Screen_Hight;
+    if (orientation == UIInterfaceOrientationLandscapeLeft ||  orientation == UIInterfaceOrientationLandscapeRight) {
+        
+        W = Screen_Hight;
+        H = Screen_Width;
     }
     
     CGFloat left = curPoint.x;
@@ -96,29 +112,78 @@ typedef enum{
     //开始吸附
     switch (mindir) {
         case Left:
+        {
+            [UIView animateWithDuration:0.3 animations:^{
+                self.superview.center = CGPointMake(self.superview.frame.size.width/2, self.superview.center.y);
             
-            self.superview.center = CGPointMake(self.superview.frame.size.width/2, self.superview.center.y);
+            }];
             
             break;
-            
+        }
         case Right:
-            
-            self.superview.center = CGPointMake(Screen_Width - self.superview.frame.size.width/2, self.superview.center.y);
+        {
+            [UIView animateWithDuration:0.3 animations:^{
+                self.superview.center = CGPointMake(Screen_Width - self.superview.frame.size.width/2, self.superview.center.y);
+            }];
             
             break;
+        }
         case Top:
-            
+        {
+            [UIView animateWithDuration:0.3 animations:^{
+                
             self.superview.center = CGPointMake(self.superview.center.x, self.superview.frame.size.height/2);
-            
+            }];
             break;
-            
+        }
         case Bottom:
-            
+        {
+            [UIView animateWithDuration:0.3 animations:^{
+                
             self.superview.center = CGPointMake(self.superview.center.x, Screen_Hight - self.superview.frame.size.height/2);
-            
+            }];
             break;
-            
+        }
         default:
+            break;
+    }
+}
+
+//屏幕颠倒时坐标交换
+- (CGPoint)UpsideDown:(CGPoint)p{
+    
+    return CGPointMake(Screen_Width - p.x, Screen_Hight - p.y);
+}
+
+//屏幕左转时坐标交换
+- (CGPoint)LandScapeLeft:(CGPoint)p{
+    
+    return CGPointMake(p.y, Screen_Width - p.x);
+}
+
+//屏幕右转时坐标交换
+- (CGPoint)LandScapeRight:(CGPoint)p{
+    
+    return CGPointMake(Screen_Hight - p.y,p.x);
+}
+
+//坐标交换，转换到屏幕旋转之前的坐标系中
+- (CGPoint)converDir:(CGPoint)p{
+    
+    //获取屏幕的方向
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    switch (orientation) {
+        case UIInterfaceOrientationLandscapeLeft:
+            return [self LandScapeLeft:p];
+            break;
+        case UIInterfaceOrientationLandscapeRight:
+            return [self LandScapeRight:p];
+            break;
+        case UIInterfaceOrientationPortraitUpsideDown:
+            return [self UpsideDown:p];
+            break;
+        default:
+            return p;
             break;
     }
 }
